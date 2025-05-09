ff7654a8e54476062b40a81eb4309216cd3e9e43
Jiaoyang Chen <yangyang19961105@gmail.com>
2025-05-09 15:25:18 +0900

    1111111111

--- client/src/components/restaurant-card/RestaurantCard.tsx
+++ client/src/components/restaurant-card/RestaurantCard.tsx
@@ -66,11 +66,6 @@ export default function RestaurantCard({ restaurant }: RestaurantCardProps) {
 
   return (
     <div className="card">
-      <h3>{restaurant.name}</h3>
-      <p>Rating: {restaurant.rating}</p>
-      <p>{restaurant.openNow ? "Currently Open" : "Not open yet"}</p>
-      <p className="address">{restaurant.address}</p>
-      <p>{restaurant.generativeSummary}</p>
       <div className="links">
         <div>
           <a href={restaurant.url}>Link</a>
