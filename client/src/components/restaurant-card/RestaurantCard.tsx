5ee551cdd57dd38c47c4a1652a50c3d30168fa46
Jiaoyang Chen <yangyang19961105@gmail.com>
2025-05-09 15:32:37 +0900

    1111111111

--- client/src/components/restaurant-card/RestaurantCard.tsx
+++ client/src/components/restaurant-card/RestaurantCard.tsx
@@ -1,7 +1,7 @@
-
-import { Restaurant, } from "../../types";
+import { useState } from "react";
+import { Restaurant, PlaceEventType } from "../../types";
 import "./styles.css";
-
+import { getUserContext } from "../../ContextProvider";
 
 interface RestaurantCardProps {
   restaurant: Restaurant;
@@ -9,15 +9,72 @@ interface RestaurantCardProps {
 
 export default function RestaurantCard({ restaurant }: RestaurantCardProps) {
 
+  const url = import.meta.env.VITE_EVENT;
 
+  let [eventLike, setEventLike] = useState(restaurant.like || false);
+  let [eventVisit, setEventVisit] = useState(restaurant.visit || false);
+  let [eventPin, setEventPin] = useState(restaurant.pin || false);
+  const user = getUserContext();
 
+  const setEventLikeClickHandler = async () => {
+    await saveEvent(
+      PlaceEventType.like, 
+      restaurant.id, 
+      eventLike === false ? true : false, 
+      setEventLike);
+  }
 
-  
+  const setEventVisitClickHandler = async () => {
+    await saveEvent(
+      PlaceEventType.visit,
+      restaurant.id,
+      eventVisit=== false ? true : false,
+      setEventVisit);
+  }
+
+  const setEventPinClickHandler = async () => {
+    await saveEvent(
+      PlaceEventType.pin,
+      restaurant.id,
+      eventPin === false ? true : false,
+      setEventPin);
+  }
+
+  async function saveEvent(eventName: PlaceEventType, restaurantId: string, state: Boolean, success: Function): Promise<void> {
+    try {
+
+      const response = await fetch(
+        `${url}/${restaurantId}/${eventName}`,
+        { 
+          method: state ? "post" : "delete",
+          headers: {
+            Authorization: user.getToken()
+          }
+        }
+      );
+
+      if (response.ok) {
+        success(state);
+      } else {
+        // setIsError(true);
+      }
+    } catch (error) {
+      // setIsError(true);
+      console.error(error);
+    }
+  }
+
   return (
     <div className="card">
+     
       <div className="links">
         <div>
           <a href={restaurant.url}>Link</a>
+        </div>
+        <div >
+            <a className="event-icon" href="#/" onClick={ setEventVisitClickHandler }>{eventVisit ? "🚙" : "🤔"}</a>
+            <a className="event-icon" href="#/" onClick={ setEventLikeClickHandler }>{ eventLike ? "❤️" : "🩶"}</a>
+            <a className="event-icon" href="#/" onClick={ setEventPinClickHandler }>{ eventPin ? "📍" : "📌"}</a>
         </div>
       </div>
     </div>
