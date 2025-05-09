648c9e9cba96ef87344ae7a7e44a90576f8a7e53
Jiaoyang Chen <yangyang19961105@gmail.com>
2025-05-09 12:33:21 +0900

    render

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
